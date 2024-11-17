54cea31ef9a0476d9b0b30a0551f934abe363e48
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-11-17 22:20:50 +0100

    Temp commit

--- WinFormsApp1/Program.cs
+++ WinFormsApp1/Program.cs
@@ -32,13 +32,13 @@ internal static class Program
             var files = Helpers.GetLaunchFiles();
             if (files.Any())
             {
-                MessageBox.Show(string.Join(Environment.NewLine, files));
-                //var fileStr = string.Join("\0\0\0\0\0", files);
-                //var stream = new NamedPipeClientStream("pipe-f7f257d8-da2f-4562-8874-84ecc9008f4f");
-                //await stream.ConnectAsync();
+                //MessageBox.Show(string.Join(Environment.NewLine, files));
+                var fileStr = string.Join("\0\0\0\0\0", files);
+                var stream = new NamedPipeClientStream("pipe-f7f257d8-da2f-4562-8874-84ecc9008f4f");
+                await stream.ConnectAsync();
 
-                //var message = Encoding.UTF8.GetBytes(fileStr).Concat(Enumerable.Range(0, 10).Select(_ => (byte)0)).ToArray();
-                //await stream.WriteAsync(message, 0, message.Length);
+                var message = Encoding.UTF8.GetBytes(fileStr).Concat(Enumerable.Range(0, 10).Select(_ => (byte)0)).ToArray();
+                await stream.WriteAsync(message, 0, message.Length);
             }
         }
     }
